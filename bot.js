6e6965cb6ba246ebc906e17b1fcb93b79b27d412
DvRocker <42909244+DvRocker@users.noreply.github.com>
2019-02-16 19:50:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,61 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "!"
+
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","member"));
+    });
+
+
+client.on('message', message => {
+         if(message.content === prefix + "closeroom") {
+                             if(!message.channel.guild) return message.reply('** This command only for servers**');
+  
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+                message.channel.overwritePermissions(message.guild.id, {
+              SEND_MESSAGES: false
+  
+                }).then(() => {
+                    message.reply("**__تم تقفيل الشات__ ✅ **")
+                });
+                  }
+      if(message.content === prefix + "openroom") {
+                          if(!message.channel.guild) return message.reply('** This command only for servers**');
+  
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+                message.channel.overwritePermissions(message.guild.id, {
+              SEND_MESSAGES: true
+  
+                }).then(() => {
+                    message.reply("**__تم فتح الشات__✅**")
+                });
+      }
+         
+});
+
+
+
+client.on('message', message => {
+   if (message.content === "!id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" + `${message.author.discriminator}`, true)
+  .addField("ID:", `${message.author.id}`, true)
+  .addField("Create At:", `${message.author.createdAt}`, true)
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
+
+
 
